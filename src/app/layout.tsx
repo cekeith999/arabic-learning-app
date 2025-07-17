ae2945d99fb92ad35f143287cfcacfcf6c043b09
cekeith999 <cekeith@usc.edu>
2025-07-16 19:32:42 -0700

    fix: minimal dashboard and layout fixes for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="endir=ltr">
+    <html lang="en" dir="ltr">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
